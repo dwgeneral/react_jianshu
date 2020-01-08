f5264dee22d490d5c400961de37f5f9357a8728c
dwgeneral <guowind@live.com>
2020-01-08 10:56:52 +0800

    Refactor: use react-redux and actionCreators to refactor related code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import store from './store'
 import Header from './common/header/index'
 import { GlobalResetStyle } from './style';
 import { GlobalIconFont } from './assets/iconfont/iconfont'
 
 function App() {
   return (
-    <div className="App">
-      <GlobalResetStyle />
-      <GlobalIconFont />
-      <Header />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <GlobalResetStyle />
+        <GlobalIconFont />
+        <Header />
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/store/actionCreators.js
@@ -0,0 +1,6 @@
+import { SEARCH_FOCUS } from './actionTypes'
+
+export const focusSearchAction = (value) => ({
+  type: SEARCH_FOCUS,
+  value
+})
